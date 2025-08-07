4e545f5fec8fc51d9d21537fc436d37b822c6959
Manantsoa Désiré Andriatsilavina <andrianemanantsoa@gmail.com>
2025-08-07 22:05:00 +0300

    Test

--- curl.py
+++ curl.py
@@ -1,7 +1,14 @@
 import os
 
-for _ in range(100):
+for i in range(100) : 
+    os.system("curl google.com")
     os.system("curl bing.com")
     os.system("curl yahoo.com")
     os.system("curl duckduckgo.com")
-    os.system("curl google.com")
+    os.system("curl baidu.com")
+    os.system("curl yandex.com")
+    os.system("curl ask.com")
+    os.system("curl aol.com")
+    os.system("curl lycos.com")
+    os.system("curl dogpile.com")
+    os.system("curl ecosia.org")
